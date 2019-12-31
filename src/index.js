18eb3fea49d13ba67d8c22f1c6bebd045e7eccaa
philopian <philopian@users.noreply.github.com>
2019-12-31 10:12:42 -0800

    added react-hook-form and references to all the fields

--- src/index.js
+++ src/index.js
@@ -1,37 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { useForm } from "react-hook-form";
 
 import "./styles.css";
 
-function handleSubmit(event) {
-  event.preventDefault();
-  console.log(event);
-}
-
 function App() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => {
+    console.log(data);
+  };
+
   return (
-    <form className="App" onSubmit={handleSubmit}>
+    <form className="App" onSubmit={handleSubmit(onSubmit)}>
       <h1>Sign up</h1>
 
       <label>First Name:</label>
-      <input type="text" name="firstname" />
+      <input type="text" name="firstname" ref={register} />
+
       <label>Last Name:</label>
-      <input type="text" name="lastname" />
+      <input type="text" name="lastname" ref={register} />
 
       <label>Gender:</label>
-      <select name="gender">
+      <select name="gender" ref={register}>
         <option>Select ...</option>
         <option value="male">Male</option>
         <option value="female">Female</option>
       </select>
 
       <label>Email:</label>
-      <input type="email" name="email" />
+      <input type="email" name="email" ref={register} />
 
       <label>About you:</label>
-      <textarea type="text" name="about" />
+      <textarea type="text" name="about" ref={register} />
 
-      <input type="submit" value="Create Account" />
+      <input
+        type="submit"
+        name="submit"
+        value="Create Account"
+        ref={register}
+      />
     </form>
   );
 }
