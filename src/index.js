b30fe6028fc53bde411cc1ce9337cc70e69c7540
philopian <philopian@users.noreply.github.com>
2019-12-31 10:23:04 -0800

    form validations

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { useForm } from "react-hook-form";
 import "./styles.css";
 
 function App() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, errors } = useForm();
   const onSubmit = data => {
     console.log(data);
   };
@@ -15,25 +15,39 @@ function App() {
       <h1>Sign up</h1>
 
       <label>First Name:</label>
-      <input type="text" name="firstname" ref={register} />
+      <input
+        type="text"
+        name="firstname"
+        ref={register({ required: true, minLength: 3 })}
+      />
+      {errors.firstname && errors.firstname.type === "required" && (
+        <p>This is required</p>
+      )}
+      {errors.firstname && errors.firstname.type === "minLength" && (
+        <p>This needs to be more than 3 characters</p>
+      )}
 
       <label>Last Name:</label>
-      <input type="text" name="lastname" ref={register} />
+      <input type="text" name="lastname" ref={register({ required: true })} />
+      {errors.lastname && <p>This is required</p>}
 
       <label>Gender:</label>
-      <select name="gender" ref={register}>
+      <select name="gender" ref={register({ required: true })}>
         <option>Select ...</option>
         <option value="male">Male</option>
         <option value="female">Female</option>
       </select>
+      {errors.gender && <p>This is required</p>}
 
       <label>Email:</label>
-      <input type="email" name="email" ref={register} />
+      <input type="email" name="email" ref={register({ required: true })} />
+      {errors.email && <p>This is required</p>}
 
       <label>About you:</label>
-      <textarea type="text" name="about" ref={register} />
+      <textarea type="text" name="about" ref={register({ required: false })} />
 
       <input
+        className="submit"
         type="submit"
         name="submit"
         value="Create Account"
